6452944fe9b09be695c1261ce1905f6d75481f04
jota_B <jeanbaptiste.alayrac@gmail.com>
2014-04-26 12:52:51 +0200

    Mise à jour manip_data et sgd

--- code/manip_data.py
+++ code/manip_data.py
@@ -1,45 +1,111 @@
 import pandas as pd
+from pandas import DataFrame, Series
 import numpy as np
-from pandas import DataFrame
+from stochastic_grad_descent import *
+import random
 
-# Load the datas
+def select_test_set(data,n=1):
+    index_user = data.index 
+    sel_ind = [random.randint(0,data[index_user[0]]-1)]
+    for j in range(n-1):
+            sel_ind = np.concatenate((sel_ind,[random.randint(0,data[index_user[0]]-1)]))
+    
+    for i in range(index_user.size-1):
+        sub_ind = [random.randint(data[index_user[i]],data[index_user[i+1]]-1)]
+        for j in range(n-1):
+            sub_ind = np.concatenate((sub_ind,[random.randint(data[index_user[i]],data[index_user[i+1]]-1)]))
+        
+        sel_ind = np.concatenate((sel_ind,sub_ind))
+           
+    return sel_ind
 
-path = '../data/ml-1m/'
 
-unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table(path + 'users.dat', sep='::', header = None,names=unames)
+if __name__=='__main__':
+    # Load the datas
+    path = '../data/ml-1m/'   
+    # Users
+    unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+    users = pd.read_table(path + 'users.dat', sep='::', header = None,names=unames)    
+    rnames = ['user_id','movie_id', 'rating', 'timestamp']
+    ratings = pd.read_table(path+'ratings.dat', sep = '::', header = None, names = rnames)    
+    mnames = ['movie_id', 'title', 'genres']
+    movies = pd.read_table(path+'movies.dat', sep ='::', header=None,names=mnames)
+    
+    data = pd.merge(pd.merge(ratings, users) ,movies)
+    mean_ratings = data.pivot_table('rating', rows='title',cols='gender',aggfunc='mean')
+    
+    mean_ratings_by_user = data.groupby('user_id')['rating'].mean()
+    mean_ratings_by_movie = data.groupby('movie_id')['rating'].mean()
+    
+    # Create the bias b_i, b_u, and mu
+    mu = data.rating.mean()
+    b_u = mean_ratings_by_user-mu
+    b_i = mean_ratings_by_movie-mu
+    
+    # Create the index of movie_id and user_id to make the link between the index
+    # and the actual names of users and movies
+    #
+    #movie_id = mean_ratings_by_movie.index
+    #user_id = mean_ratings_by_user.index
+    
+    # Create the matrix that we have to complete
+    #big_matrix = data.pivot_table('rating',rows='movie_id',cols='user_id')
+    
+    #Create the tuple of index on which we want to optimize our objective function
+    #Note : we sort the values by the user id in order to ease the creation of the 
+    #test and train dataset
+    triplet = DataFrame(data,columns=['user_id','movie_id','rating'])
+    list_triplet = triplet.sort_index(by='user_id').values
+    
+    
+    # Number of ratings by user
+    ratings_by_user_cumsum = data.groupby('user_id').size().cumsum()
+    
+    # Size of the database (number of ratings)
+    n_ratings = int(list_triplet.shape[0])
+    
+    
+    
+    # Creation of the map between index and movie and user id
+    movie_id_val = data.movie_id.unique()
+    movie_id_val.sort()
+    movie_to_index = Series(np.arange(data.movie_id.unique().size),index=movie_id_val)
+    user_id_val = data.user_id.unique()
+    user_id_val.sort()
+    user_to_index = Series(np.arange(data.user_id.unique().size),index=user_id_val)
+    
+    # Number of ratings by user
+    ratings_by_user_cumsum = data.groupby('user_id').size().cumsum()
+    # Creation of the training set and test set (a refaire mieux)
+    
+    #Create an index to choose a test set
+    n_movie_by_user = 2
+    ind_test = select_test_set(ratings_by_user_cumsum,n=n_movie_by_user)
+    
+    triplet_test = list_triplet[ind_test,:]
+    triplet_train = np.delete(list_triplet,ind_test,0)
+    
+    
+    alpha = 0.1
+    gamma = 0.1
+    
+    # Toy Example
+    #movie_to_index = np.arange(5)
+    #user_to_index = np.arange(3)
+    #list_triplet = np.array([[0,0,5],[0,2,3],[0,4,5],[1,1,4],[1,4,5],[2,2,5],[2,3,4],[2,4,1]])
+    #mu = 4
+    #dev_mean_u = np.array([0.333,0.5,-1])
+    #b_u = Series(dev_mean_u,np.arange(3))
+    #
+    #dev_mean_i = np.array([1,0,0,0,-0.333])
+    #b_i = Series(dev_mean_i,np.arange(5))
+    
+    #L,R = simple_sgd(movie_to_index,user_to_index,b_u,b_i,mu,list_triplet,alpha,gamma)
 
-rnames = ['user_id','movie_id', 'rating', 'timestamp']
-ratings = pd.read_table(path+'ratings.dat', sep = '::', header = None, names = rnames)
 
-mnames = ['movie_id', 'title', 'genres']
-movies = pd.read_table(path+'movies.dat', sep ='::', header=None,names=mnames)
 
-data = pd.merge(pd.merge(ratings, users) ,movies)
-mean_ratings = data.pivot_table('rating', rows='title',cols='gender',aggfunc='mean')
 
-mean_ratings_by_user = data.groupby('user_id')['rating'].mean()
-mean_ratings_by_movie = data.groupby('movie_id')['rating'].mean()
 
-# Create the bias b_i, b_u, and mu
-mu = data.rating.mean()
-b_u = mean_ratings_by_user-mu
-b_i = mean_ratings_by_movie-mu
-
-# Create the index of movie_id and user_id to make the link between the index
-# and the actual names of users and movies
-
-movie_id = mean_ratings_by_movie.index
-user_id = mean_ratings_by_user.index
-
-# Create the matrix that we have to complete
-big_matrix = data.pivot_table('rating',rows='movie_id',cols='user_id')
-
-#Create the tuple of index on which we want to optimize our objective function
-list_triplet = DataFrame(data,columns=['user_id','movie_id','rating']).values
-
-# Size of the database (number of ratings)
-n_ratings = list_triplet.shape
 
 
 
--- code/stochastic_grad_descent.py
+++ code/stochastic_grad_descent.py
@@ -1,2 +1,56 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+#from matplotlib.pyplot import pause
+import random
+
+def simple_sgd(index_i,index_u,b_u,b_i,mu,triplet,alpha,gamma,norm='att_paper',r=5):
+    
+    n_u = index_u.shape[0]
+    n_i = index_i.shape[0]
+    # Prepare the factorization matrix
+    L = np.random.random([n_u,r])
+    R = np.random.random([n_i,r])
+    
+    if norm=='att_paper':
+        
+        n_total = int(triplet.shape[0])
+        #n_iter = int(n_total*np.log(n_total))    
+        n_iter = 100000
+        
+        #val = compObjFunc(mu,b_u,b_i,index_u,index_i,L,R,triplet,alpha)
+        #obj = np.array([val])
+        
+        for data in range(n_iter):
+            
+            ind = random.randint(0,n_total-1)
+            u = index_u[triplet[ind,0]]
+            i = index_i[triplet[ind,1]]
+            e = triplet[ind,2]-mu-b_i[triplet[ind,1]]-b_u[triplet[ind,0]]-np.dot(L[u,:],R[i,:])
+            
+            L[u,:] = L[u,:]+gamma*(e*R[i,:]-alpha*L[u,:])
+            R[i,:] = R[i,:]+gamma*(e*L[u,:]-alpha*R[i,:])
+            
+            #val = compObjFunc(mu,b_u,b_i,index_u,index_i,L,R,triplet,alpha)
+            #obj = np.concatenate((obj,[val]))
+            
+        return L,R
+        
+    else:
+        print('You must specified a valid norm!')
+        
+def compObjFunc(mu,b_u,b_i,index_u,index_i,L,R,triplet,alpha):
+    
+    n_total = int(triplet.shape[0])
+    obj = 0    
+   
+    for ind in range(n_total):
+        u = index_u[triplet[ind,0]]
+        i = index_i[triplet[ind,1]]
+        e = triplet[ind,2]-mu-b_i[triplet[ind,1]]-b_u[triplet[ind,0]]-np.dot(L[u,:],R[i,:])    
+        obj = obj+e*e+alpha*(np.dot(L[u,:],L[u,:])+np.dot(R[i,:],R[i,:]))
+    
+    return obj
+    
+        
+                  
