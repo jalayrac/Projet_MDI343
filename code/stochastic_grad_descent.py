7a5ce7014ae7fa07c1d8ec7e3c1ef0a3a6184b0a
nkeriven <nicolas.keriven@gmail.com>
2014-04-30 17:58:14 +0200

    jellyfish, petites modifs

--- code/stochastic_grad_descent.py
+++ code/stochastic_grad_descent.py
@@ -3,23 +3,26 @@
 import numpy as np
 #from matplotlib.pyplot import pause
 import random
+import time
 
-def simple_sgd(b_u,b_i,mu,triplet,alpha,gamma,norm='att_paper',r=50):
+def simple_sgd(b_u,b_i,mu,triplet,alpha,gamma,norm='att_paper',r=50,compute_obj=False):
     
     n_u = b_u.shape[0]
     n_i = b_i.shape[0]
     # Prepare the factorization matrix
     L = np.random.random([n_u,r])
     R = np.random.random([n_i,r])
-    
+    obj=[]
     if norm=='att_paper':
         
         n_total = int(triplet.shape[0])
         n_iter = int(n_total*np.log(n_total))    
         
-        #val = compObjFunc(mu,b_u,b_i,index_u,index_i,L,R,triplet,alpha)
-        #obj = np.array([val])
+        if compute_obj:
+            obj.append(compObjFunc(mu,b_u,b_i,L,R,triplet,alpha))
+
         e = 0
+        tic=time.clock()
         for data in range(n_iter):
             
             ind = random.randint(0,n_total-1)
@@ -27,10 +30,14 @@ def simple_sgd(b_u,b_i,mu,triplet,alpha,gamma,norm='att_paper',r=50):
             t = np.array(L[triplet[ind,0],:], copy=True)
             L[triplet[ind,0],:] = L[triplet[ind,0],:]+gamma*(e*R[triplet[ind,1],:]-alpha*L[triplet[ind,0],:])
             R[triplet[ind,1],:] = R[triplet[ind,1],:]+gamma*(e*t-alpha*R[triplet[ind,1],:])
-            #val = compObjFunc(mu,b_u,b_i,index_u,index_i,L,R,triplet,alpha)
-            #obj = np.concatenate((obj,[val]))
             
-        return L,R
+            if (data%n_total==0):
+                print('Epoch '+str(time.clock()-tic))
+                tic=time.clock()
+                if (compute_obj):
+                    obj.append(compObjFunc(mu,b_u,b_i,L,R,triplet,alpha))
+            
+        return L,R,obj
         
     else:
         print('You must specified a valid norm!')
